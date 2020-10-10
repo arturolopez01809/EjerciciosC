431c202edbe8d3c20f3c8563f46fa63f4d9d42a5
arturolopez01809@gmail.com <arturolopez01809@gmail.com>
2020-10-10 13:30:45 +0200

    Ejercicios 4 y 5 terminados

--- Ejercicio4/Ejercicio4/Program.cs
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -8,8 +8,47 @@ namespace Ejercicio4
 {
     class Program
     {
+
+        static void CompararEsPar(int numero1, int numero2)
+        {
+            bool par_numero1 = false;
+            bool par_numero2 = false;
+
+
+            if(numero1 % 2 == 0)
+            {
+                par_numero1 = true;
+            } 
+
+            if(numero2 % 2 == 0)
+            {
+                par_numero2 = true;
+            }
+
+            if(par_numero1 && par_numero2){
+                Console.WriteLine("Ambos números son pares");
+            } else
+            {
+                Console.WriteLine("Ambos números NO son pares");
+            }
+        }
         static void Main(string[] args)
         {
+            int numero1 = 0;
+            int numero2 = 0;
+
+            Console.WriteLine("Introduzca un número entero: ");
+
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduzca otro número entero: ");
+
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            CompararEsPar(numero1, numero2);
+
+            Console.ReadLine(); 
+
         }
     }
 }
---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+
+        static int GenerarNumeroAleatorio()
+        {
+            int numero = 0;
+
+            Random random = new Random();
+
+            numero = random.Next(21);
+
+            return numero;
+        }
+
+        static bool CompararNumeros(int numero_usuario, int numero_aleatorio)
+        {
+            bool resultado = false;
+
+            if(numero_usuario == numero_aleatorio)
+            {
+                resultado = true;
+
+                Console.WriteLine("ACERTASTE!!!!!!!");
+            } else if(numero_usuario > numero_aleatorio)
+            {
+                Console.WriteLine("EL número es demasiado grande");
+            } else if(numero_usuario < numero_aleatorio)
+            {
+                Console.WriteLine("El número es demasiado pequeño");
+            }
+
+            return resultado;
+        }
+
+        static void Main(string[] args)
+        {
+            int numero_usuario = 0;
+            int numero_aleatorio = 0;
+
+            bool numero_encontrado = false;
+
+            Console.WriteLine("Introduzca un número entero entre 0-20: ");
+            numero_usuario = Convert.ToInt32(Console.ReadLine());
+
+            numero_aleatorio = GenerarNumeroAleatorio();
+
+            int contador = 0;
+
+            do
+            {
+
+                Console.WriteLine("Introduzca un número entero entre 0-20: ");
+                numero_usuario = Convert.ToInt32(Console.ReadLine());
+
+                numero_encontrado = CompararNumeros(numero_usuario, numero_aleatorio);
+                contador++;
+
+            } while (!numero_encontrado || contador >= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
