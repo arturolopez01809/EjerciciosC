8060634a996685e88aa32cc6b4899151af8b1aae
arturolopez01809@gmail.com <arturolopez01809@gmail.com>
2020-10-10 19:30:50 +0200

    Ejercicio 6 terminado

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string vector_strings;
+
+            Console.WriteLine("Introduce una serie de números: ");
+
+            vector_strings = Console.ReadLine();
+
+            int contador = 0;
+
+            foreach (char i in vector_strings)
+            {
+                if(i == '7')
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("La cadena insertada tiene " + contador + " sietes");
+            Console.ReadLine();
+
+        }
+    }
+}
